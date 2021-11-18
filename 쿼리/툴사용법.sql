51f3f70e7db90c9fbc5b9314085e26c535647ae7
ggoomter <ggoomter@gmail.com>
2021-11-18 23:24:29 +0900

    디비버, mysql 변수, rownum

--- 쿼리/툴사용법.sql
+++ 쿼리/툴사용법.sql
@@ -1,12 +1,15 @@
 DBeaver
 https://meyouus.tistory.com/52
 - CTRL + ENTER : 현재 쿼리 실행
-- ALT + X : 현재 스크립트 실행
+- ALT + X : 현재 블록지정된 스크립트 실행
 - CTRL SHIFT E : 현재 쿼리 실행계획 설명
 - CTRL + SPACE : 자동완성
 - CTRL + SHIFT + F : 자동 줄맞춤
 - Ctrl shift x : 대문자로 변경
 - Ctrl shift y : 소문자로 변경
+- F4 : 테이블 정의
+- 쉽게 쿼리작성하는법(컬럼가져오는법) : SELECT한 결과 테이블 우클릭 - SQL생성
+
 
 
 TAB : 기록/격자 모드 전환
